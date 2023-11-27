6b33879e4cd82181321ff2ba8718025b28dc2d74
froaklala <max.wenddel.alvarenga2020@gmail.com>
2023-11-27 16:25:48 -0300

    trabalhando no carrossel

---
+++ assets/main.js
@@ -0,0 +1,15 @@
+const imagensCarrossel = document.getElementsByClassName("img-carrossel");
+const botaoAvancar = document.getElementById("avancar")
+function mostrarImagem(indice){
+    imagensCarrossel[indice].setAttribute("class", "img-carrossel display-block")
+}
+function esconderImagens(){
+    for(let i=0; i<=imagensCarrossel.length-1; i++){
+        imagensCarrossel[i].setAttribute("class", "img-carrossel display-none")   
+    }
+}
+let indice=0
+window.onload=()=>{
+    esconderImagens()
+    mostrarImagem()
+}
